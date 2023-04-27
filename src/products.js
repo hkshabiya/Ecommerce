e1b8dd340993a52aa28d9fb2f8022adb091343e0
hkshabiya <hkshabiya@gmail.com>
2023-04-27 23:07:47 +0530

    Added my project

---
+++ src/products.js
@@ -0,0 +1,72 @@
+import produt1 from "./assets/1.jpg"
+import produt2 from "./assets/2.jpg"
+import produt3 from "./assets/3.avif"
+import produt4 from "./assets/4.webp"
+import produt5 from "./assets/5.jpg"
+import produt6 from "./assets/6.jpg"
+import produt7 from "./assets/7.webp"
+import produt8 from "./assets/8.jpg"
+import produt9 from "./assets/9.webp"
+import produt10 from "./assets/10.jpg"
+export const PRODUCTS =[
+    {
+        id:1,
+        productName:"IPhone",
+        price:56999.0,
+        productImage:produt1,
+    },
+    {
+        id:2,
+        productName:"Camera",
+        price:600000.0,
+        productImage:produt2,
+    },
+    {
+        id:3,
+        productName:"Laptop",
+        price:70999.0,
+        productImage:produt3,
+    },
+    {
+        id:4,
+        productName:"Laptop",
+        price:66099.0,
+        productImage:produt4,
+    },
+    {
+        id:5,
+        productName:"Camera",
+        price:999000.0,
+        productImage:produt5,
+    },
+    {
+        id:6,
+        productName:"T Shirt",
+        price:899.0,
+        productImage:produt6,
+    },
+    {
+        id:7,
+        productName:"Shirt",
+        price:799.0,
+        productImage:produt7,
+    },
+    {
+        id:8,
+        productName:"Cap",
+        price:299.0,
+        productImage:produt8,
+    },
+    {
+        id:9,
+        productName:"Hat",
+        price:499.0,
+        productImage:produt9,
+    },
+    {
+        id:10,
+        productName:"IPhone",
+        price:23999.0,
+        productImage:produt10,
+    }, 
+];
